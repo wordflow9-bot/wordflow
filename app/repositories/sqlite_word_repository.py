18a23ea089088ea8c0aea43ffebadf88ff6b54e5
Max <max@Maxs-MacBook-Air.local>
2025-12-13 10:41:03 +0300

    "changes"

--- app/repositories/sqlite_word_repository.py
+++ app/repositories/sqlite_word_repository.py
@@ -1,5 +1,6 @@
 import sqlite3
 
+<<<<<<< Updated upstream
 DB_LOCATION = "database.db"
 
 def get_connection():
@@ -52,3 +53,37 @@ def query_performed(word_id: int, correct: bool):
         )
     con.commit()
     con.close()
+=======
+
+# DB_LOCATION = "database.db"
+# тут пока написана херня
+
+
+class SQLiteWordRepository:
+    def __init(self, db_path = "database.db"):
+        con = sqlite3.connect(DB_LOCATION)
+        cur = con.cursor()
+        
+    def initialize():
+        cur.execute("""CREATE TABLE words(id INTEGER PRIMARY KEY AUTOINCREMENT, 
+                    user_id INTEGER, word TEXT, translation TEXT, total_cnt INTEGER, correct_cnt INTEGER)""")
+        con.commit()
+
+
+    def add_word(user_id : int, word : str, translation : str):
+        cur.execute("INSERT INTO words (user_id, word, translation, total_cnt, correct_cnt) VALUES ({}, '{}', '{}', 0, 0)"
+                    .format(user_id, word, translation))
+        con.commit()
+
+
+    def get_random_word_data(user_id : int):
+        cur.execute("SELECT (word_id, word, translation) FROM word WHERE user_id={} ORDER BY RANDOM() LIMIT 1".format(user_id))
+        return cur.fetchone()
+
+
+    def query_performed(word_id : int, correct : bool):
+        cur.execute("UPDATE words SET total_cnt = total_cnt + 1 WHERE id={}".format(word_id))
+        if correct:
+            cur.execute("UPDATE words SET correct_cnt = correct_cnt + 1 WHERE id={}".format(word_id))
+        con.commit()
+>>>>>>> Stashed changes
