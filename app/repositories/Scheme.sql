3d46a7ac9d14322ff04b8b2d10902e6a5a629a3c
mosannikov-source <mosannikov@gmail.com>
2025-12-13 13:45:13 +0300

    Update Scheme.sql

--- app/repositories/Scheme.sql
+++ app/repositories/Scheme.sql
@@ -1,8 +1,11 @@
 CREATE TABLE IF NOT EXISTS words (
     id INTEGER PRIMARY KEY AUTOINCREMENT,
+    user_id INTEGER,
     word TEXT NOT NULL,
     translation TEXT NOT NULL,
-    mastery_level INTEGER NOT NULL DEFAULT 0,
-    created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
-    updated_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP
+    total_cnt INTEGER, 
+    correct_cnt INTEGER,
+    mastery_level INTEGER NOT NULL DEFAULT 0 -- в перспективе mastery level зависит не только от total_cnt и correct_cnt, но и от времени с последнего правильного ответа/теста со словом
+    -- created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+    -- updated_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP
 );
