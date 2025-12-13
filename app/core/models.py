44a97d57a9c16ee79ada197d397973914e6ee373
mosannikov-source <mosannikov@gmail.com>
2025-12-13 14:28:04 +0300

    Update models.py

--- app/core/models.py
+++ app/core/models.py
@@ -3,10 +3,14 @@ from typing import Optional
 # from datetime import datetime
 
 @dataclass
-class Word:
-    id: Optional[int]
+class UserWord:
+    user_id: Optional[int]
+    word_id: Optional[int]
     word: str
     translation: str
+    total_cnt: int = 0
+    correct_cnt: int = 0
     mastery_level: int = 0
     # created_at: datetime = None
     # updated_at: datetime = None
+
