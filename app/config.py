190b12536e723275262c61041391492628b55ad2
Max <max@Maxs-MacBook-Air.local>
2026-03-07 13:50:30 +0300

    Обновление конфигурации и добавление тестового изображения

--- app/config.py
+++ app/config.py
@@ -1,6 +1,9 @@
 # app/config.py
 from pydantic_settings import BaseSettings
 from pathlib import Path
+import os
+
+os.environ["GOOGLE_APPLICATION_CREDENTIALS"] = "mindful-marking-474111-n5-9e4db07e6c18.json"
 
 class Settings(BaseSettings):
     telegram_bot_token: str
--- test_config.py
+++ test_config.py
@@ -1,5 +1,20 @@
 # test_config.py
 from app.config import settings
+from google.cloud import vision
+from pathlib import Path
+import os
 
-print("Токен бота:", settings.telegram_bot_token[:10] + "...")
-print("База данных:", settings.database_url)
+os.environ["GOOGLE_APPLICATION_CREDENTIALS"] = "mindful-marking-474111-n5-9e4db07e6c18.json"
+
+client = vision.ImageAnnotatorClient()
+
+with open("tests/test.jpg", "rb") as f:
+    content = f.read()
+
+image = vision.Image(content=content)
+response = client.text_detection(image=image)
+
+print(response.full_text_annotation.text)
+
+# print("Токен бота:", settings.telegram_bot_token[:10] + "...")
+# print("База данных:", settings.database_url)
