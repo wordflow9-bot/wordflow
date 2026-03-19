5f5fb63e8ecce35e45664139371eaa5ab7ae970f
mosannikov-source <mosannikov@gmail.com>
2026-03-19 23:02:59 +0300

    Refactor error handling in extract_text function
    Refactor error handling in extract_text function to uncomment and improve clarity.

--- app/services/ocr/ocr_service.py
+++ app/services/ocr/ocr_service.py
@@ -4,30 +4,30 @@ from .fallback_easyocr import extract_text_easyocr
 
 
 def extract_text(input_image_bytes: bytes) -> dict:
-    # try: #WORKS
-    #     text = extract_text_google(input_image_bytes)
-    #     if text and text.strip():
-    #         return {
-    #             "engine": "google",
-    #             "text": text
-    #         }
-    #     elif not text:
-    #         return {
-    #             "engine": "google",
-    #             "text": "not found"
-    #         }
+    try: #WORKS
+        text = extract_text_google(input_image_bytes)
+        if text and text.strip():
+            return {
+                "engine": "google",
+                "text": text
+            }
+        elif not text:
+            return {
+                "engine": "google",
+                "text": "not found"
+            }
 
-    # except ResourceExhausted:
-    #     # print("Resource Exausted") #debug
-    #     pass
+    except ResourceExhausted:
+        # print("Resource Exausted") #debug
+        pass
 
-    # except GoogleAPIError:
-    #     # print("Google API Error") #debug
-    #     pass
+    except GoogleAPIError:
+        # print("Google API Error") #debug
+        pass
 
-    # except Exception:
-    #     # print("Unkown Exception") #debug
-    #     pass 
+    except Exception:
+        # print("Unkown Exception") #debug
+        pass 
 
     try:
         text = extract_text_easyocr(input_image_bytes)
